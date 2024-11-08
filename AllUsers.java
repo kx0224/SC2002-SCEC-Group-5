587325eb645d6d66eae1af6168a463bde710386f
kx0224 <167541621+kx0224@users.noreply.github.com>
2024-11-08 16:08:32 +0800

    Update AllUsers.java
    change from array to arraylist

--- AllUsers.java
+++ AllUsers.java
@@ -1,61 +1,45 @@
-package allusers;
-
-import java.io.BufferedReader;  
-import java.io.FileReader;  
-import java.io.IOException;  
-
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class AllUsers {
-	
-	public User[] users = new User[1000];
-	
-	public AllUsers() {
-		initialiseUsers();
-	}
-	
-	public void initialiseUsers() {
-		//parsing a CSV file into Scanner class constructor  
-		String line = "";  
-		String splitBy = ",";  
-		int position = 0;
 
-		try   
-		{  
-		//parsing a CSV file into BufferedReader class constructor  
-		BufferedReader br = new BufferedReader(new FileReader("Staff_List.csv"));  
-		while ((line = br.readLine()) != null)   //returns a Boolean value  
-		{  
-		String[] newuser = line.split(splitBy);    // use comma as separator  
-		User userobj=new User(newuser[0], newuser[2]);
-		users[position] = userobj;
-		position++;
-		}
-		}   
-		catch (IOException e)   
-		{  
-		e.printStackTrace();  
-		}  
-		
-		
-		line = "";  
-		splitBy = ",";  
-		try   
-		{  
-		//parsing a CSV file into BufferedReader class constructor  
-		BufferedReader br = new BufferedReader(new FileReader("Paient_List.csv"));  
-		while ((line = br.readLine()) != null)   //returns a Boolean value  
-		{  
-		String[] newuser = line.split(splitBy);    // use comma as separator  
-		User userobj=new User(newuser[0], "Patient");
-		users[position] = userobj;
-		position++;
-		}
-		}   
-		catch (IOException e)   
-		{  
-		e.printStackTrace();  
-		}
-		
-	}
-	
+    public ArrayList<User> users = new ArrayList<>();
+
+    public AllUsers() {
+        initialiseUsers();
+    }
+
+    public void initialiseUsers() {
+        String line = "";
+        String splitBy = ",";
+
+        try {
+            // Reading the Staff_List.csv file
+            BufferedReader br = new BufferedReader(new FileReader("Staff_List.csv"));
+            while ((line = br.readLine()) != null) {
+                String[] newuser = line.split(splitBy);  // Use comma as separator
+                User userobj = new User(newuser[0], newuser[2]);
+                users.add(userobj);
+            }
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        line = "";
+        try {
+            // Reading the Patient_List.csv file
+            BufferedReader br = new BufferedReader(new FileReader("Patient_List.csv"));
+            while ((line = br.readLine()) != null) {
+                String[] newuser = line.split(splitBy);  // Use comma as separator
+                User userobj = new User(newuser[0], "Patient");
+                users.add(userobj);
+            }
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
