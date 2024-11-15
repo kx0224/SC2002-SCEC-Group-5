2161ee38f033e7824275b0a797a2e5efccedfe35
newguyplaying <149993929+newguyplaying@users.noreply.github.com>
2024-11-15 20:05:08 +0800

    Fixed a glaring case issue.

--- Appointment.java
+++ Appointment.java
@@ -1,30 +1,28 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-
+package sc2002.group5.oopprojectsrc;
 public class Appointment{
-    int timeslot;
+    protected int timeslot;
     int[] datearray=new int[2]; 
     String pname;
     String doctorname;
-    ArrayList<String> outcomemedications = new ArrayList<>();
-    ArrayList<Float> medicationquantity= new ArrayList<>();
+    String[] outcomemedications = null;
+    Float[] medicationquantity= null;
     String treatmentype;
     String consultationnotes;
     int acceptedstatus;
-    Appointment(int ts, String ipdate, String pm, String dm){
+    Appointment(int ts, String ipdate, String pm, String dm, int acceptedstatus){
         this.timeslot=ts;
         this.pname=pm;
-        this.acceptedstatus=0;
+        this.acceptedstatus=acceptedstatus;
         this.doctorname=dm;
         this.treatmentype=null;
-        this.consultationnotes=pending;
+        this.consultationnotes=null;
         this.datearray=datestringtoarray(ipdate);
     }
-    Appointment(int ts, String ipdate, String pm,  ArrayList<String> meds, ArrayList<Float> medamt, String trtment, String cs, String dm, int as){
+    Appointment(int ts, String ipdate, String pm, String dm, String[] meds, Float[] medamt, String trtment, String cs, int as){
         this.timeslot=ts;
         this.pname=pm;
         this.acceptedstatus=as;
-        this.outcomemedication=meds;
+        this.outcomemedications=meds;
         this.doctorname=dm;
         this.medicationquantity=medamt;
         this.treatmentype=trtment;
@@ -32,12 +30,18 @@ public class Appointment{
         this.datearray=datestringtoarray(ipdate);
     }
     public int[] datestringtoarray(String dateString){
-        String[] dateParts = dateString.split("/");
+        String[] dateParts = dateString.split(";");
         int[] dateArray=new int[2];
         dateArray[0] = Integer.parseInt(dateParts[0]); // Convert the first part to an integer (month)
         dateArray[1] = Integer.parseInt(dateParts[1]); // Convert the second part to an integer (day)
         return dateArray;
     
+    }
+    public String datearraytoString(){
+        String[] datestring=new String[2];
+        datestring[0]=String.valueOf(datearray[0]);
+        datestring[1]=String.valueOf(datearray[1]);
+        return String.join(";",datestring);
     }
 
     public void showdetails(){
@@ -53,8 +57,8 @@ public class Appointment{
         else if (acceptedstatus==-1)System.out.println("Outcome: Appointment declined");
         else {
             System.out.println("Medications issued:");
-            for (int i=0;i<outcomemedications.size();i++){
-                System.out.printf("%s  |  %d  g \n", outcomemedications.get(i), medicationquantity,get(i));
+            for (int i=0;i<outcomemedications.length;i++){
+                System.out.printf("%s  |  %d  g \n", outcomemedications[i], medicationquantity[i]);
             }
             System.out.println("Treatment type:"+treatmentype);
             System.out.println("Consultation notes: \n"+ consultationnotes);
