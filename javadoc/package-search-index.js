c550bca87666454e998d5d9a6b50979c47b0c490
Tableunstable <128674360+Tableunstable@users.noreply.github.com>
2024-11-21 19:26:08 +0800

    Add files via upload

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"controllers"},{"l":"managers"},{"l":"menu"},{"l":"models"},{"l":"service"},{"l":"users"}];updateSearchResults();
