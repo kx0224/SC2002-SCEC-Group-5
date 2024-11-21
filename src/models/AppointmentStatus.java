5f6bae687b447b822cd87ff25e638e4469d31189
Tableunstable <128674360+Tableunstable@users.noreply.github.com>
2024-11-21 19:17:30 +0800

    Delete src directory

--- src/models/AppointmentStatus.java
+++
@@ -1,9 +0,0 @@
-package models;
-
-public enum AppointmentStatus {
-    PENDING,
-    COMPLETE,
-    CONFIRMED,
-    CANCELLED,
-    SCHEDULED
-}
--- src/models/UserRoles.java
+++
@@ -1,9 +0,0 @@
-package models;
-
-public enum UserRoles {
-    DOCTOR, 
-    PATIENT, 
-    PHARMACIST, 
-    ADMIN;
-
-}
